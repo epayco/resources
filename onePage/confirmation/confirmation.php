c252a762a7fabc3f3ee34d43e2b1d825fb6ca314
Dayancmm <83416039+Dayancmm@users.noreply.github.com>
2021-08-26 17:58:11 -0500

    Update confirmation.php
    Se añade validaciones para verificar que el número de orden y valor coinciden con los valores recibidos en la transacción

--- onePage/confirmation/confirmation.php
+++ onePage/confirmation/confirmation.php
@@ -19,11 +19,16 @@ $x_signature      = $_REQUEST['x_signature'];
 
 $signature = hash('sha256', $p_cust_id_cliente . '^' . $p_key . '^' . $x_ref_payco . '^' . $x_transaction_id . '^' . $x_amount . '^' . $x_currency_code);
 
+// obtener invoice y valor en el sistema del comercio
+$numOrder = '2531'; // Este valor es un ejemplo se debe reemplazar con el número de orden que tiene registrado en su sistema
+$valueOrder = 10000;  // Este valor es un ejemplo se debe reemplazar con el valor esperado de acuerdo al número de orden del sistema del comercio
+
 $x_response     = $_REQUEST['x_response'];
 $x_motivo       = $_REQUEST['x_response_reason_text'];
 $x_id_invoice   = $_REQUEST['x_id_invoice'];
 $x_autorizacion = $_REQUEST['x_approval_code'];
-
+// se valida que el número de orden y el valor coincidan con los valores recibidos
+if ($x_id_invoice === $numOrder && $x_amount === $valueOrder) {
 //Validamos la firma
 if ($x_signature == $signature) {
     /*Si la firma esta bien podemos verificar los estado de la transacción*/
@@ -45,8 +50,10 @@ if ($x_signature == $signature) {
             # code transacción fallida
             //echo "transacción fallida";
             break;
-
+        }
+    } else {
+        die("Firma no valida");
     }
 } else {
-    die("Firma no valida");
+    die("numero de orden o valor pagado no coinsiden");
 }
