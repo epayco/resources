b2e3632003291515caa68b5c4375ce8a17320ffa
RicardoSaldarriagaPayco <ricardo.saldarriaga@epayco.com>
2022-08-17 08:55:49 -0500

    feat:se actualiza version de react para integracion personalizada

--- epayco-react/src/App.js
+++ epayco-react/src/App.js
@@ -2,14 +2,14 @@ import React, {Fragment} from 'react';
 import Succes from './components/Succes';
 
 function App(){
-return (
-  <Fragment>
-    <div>
-      pagar con epayco
-    </div>
-      < Succes/>
-  </Fragment>
-)
+    return (
+      <Fragment>
+        <div>
+          pagar con epayco
+        </div>
+          < Succes/>
+      </Fragment>
+    )
 }
 
 export default App;
